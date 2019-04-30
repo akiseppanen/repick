2bb7997351953d867fe8169993afec517b9790be
Aki Seppänen <aki.seppanen@gofore.com>
2019-04-30 11:43:07 +0300

    Refactor to hooks

--- packages/repick-react/src/repick.ts
+++ packages/repick-react/src/repick.ts
@@ -1,14 +1,13 @@
 import * as startOfDay from 'date-fns/start_of_day'
 import * as React from 'react'
 import {
-  Action,
   buildCalendar,
   Calendar,
   CalendarDay,
   defaultOptions,
   keyToAction,
+  Options,
   reducer,
-  State,
 } from 'repick-core'
 
 export interface CalendarProps {
@@ -31,14 +30,14 @@ export interface DateProps {
   ref: (el: HTMLElement | null) => void
 }
 
-export interface ChildProps extends Calendar {
-  selectDate: (date: string | number | Date) => State
-  prevDay: () => State
-  nextDay: () => State
-  prevWeek: () => State
-  nextWeek: () => State
-  prevMonth: () => State
-  nextMonth: () => State
+export interface Props extends Calendar {
+  selectDate: (date: string | number | Date) => void
+  prevDay: () => void
+  nextDay: () => void
+  prevWeek: () => void
+  nextWeek: () => void
+  prevMonth: () => void
+  nextMonth: () => void
   getCalendarProps: () => CalendarProps
   getDateProps: (calendarDay: CalendarDay) => DateProps
   getNextMonthProps: () => MonthProps
@@ -48,12 +47,11 @@ export interface ChildProps extends Calendar {
   setFocusToDate: (date: Date) => void
 }
 
-interface Props {
-  children: (s: ChildProps) => React.ReactChild
+interface RepickProps {
   onChange?: (date: Date) => void
   onUpdate?: (calendar: Calendar) => void
   weekStartsOn?: number
-  locale?: string
+  locale?: object
   format?: string
   date?: Date
   initialDate?: Date
@@ -61,140 +59,143 @@ interface Props {
   initialSelected?: Date
 }
 
-export default class Repick extends React.Component<Props, State> {
-  public static getDerivedStateFromProps(props: Props, state: State) {
-    return {
-      date: props.date || state.date,
-      selected: props.selected || state.selected,
-    }
+interface Children {
+  children: (s: Props) => React.ReactElement
+}
+
+export const useRepick = (props: RepickProps): Props => {
+  const dateRefs: Record<string, HTMLElement> = {}
+
+  const initialState = {
+    date:
+      props.initialDate ||
+      props.initialSelected ||
+      props.selected ||
+      startOfDay(new Date()),
+    selected: props.initialSelected || null,
   }
 
-  private dateRefs: Record<string, HTMLElement> = {}
+  const [dispatcherState, dispatch] = React.useReducer(reducer, initialState)
 
-  constructor(props: Props) {
-    super(props)
-
-    this.state = {
-      date:
-        this.props.initialDate ||
-        this.props.initialSelected ||
-        this.props.selected ||
-        startOfDay(new Date()),
-      selected: this.props.initialSelected || null,
-    }
+  const state = {
+    date: props.date || dispatcherState.date,
+    selected: props.selected || dispatcherState.selected,
   }
 
-  public render() {
-    const { children } = this.props
-    if (children) {
-      return children({
-        ...this.buildCalendar(),
-        selectDate: (date: string | number | Date) =>
-          this.dispatch({ type: 'SelectDate', date }),
-        prevDay: () => this.dispatch({ type: 'PrevDay' }),
-        nextDay: () => this.dispatch({ type: 'NextDay' }),
-        prevWeek: () => this.dispatch({ type: 'PrevWeek' }),
-        nextWeek: () => this.dispatch({ type: 'NextWeek' }),
-        prevMonth: () => this.dispatch({ type: 'PrevMonth' }),
-        nextMonth: () => this.dispatch({ type: 'NextMonth' }),
-        getCalendarProps: this.getCalendarProps,
-        getDateProps: this.getDateProps,
-        getNextMonthProps: this.getNextMonthProps,
-        getPrevMonthProps: this.getPrevMonthProps,
-        handleKeyDown: this.handleKeyDown,
-        setFocusToCalendar: this.setFocusToCalendar,
-        setFocusToDate: this.setFocusToDate,
-      })
-    }
-  }
-
-  private setFocusToDate = (date: Date) => {
+  const setFocusToDate = (date: Date) => {
     window.requestAnimationFrame(() => {
-      if (this.dateRefs[date.toISOString()]) {
-        this.dateRefs[date.toISOString()].focus()
+      if (dateRefs[date.toISOString()]) {
+        dateRefs[date.toISOString()].focus()
       }
     })
   }
 
-  private setFocusToCalendar = () => {
+  React.useEffect(
+    () => {
+      setFocusToDate(state.date)
+    },
+    [state.date],
+  )
+
+  React.useEffect(
+    () => {
+      if (props.onChange && state.selected) {
+        props.onChange(state.selected)
+      }
+    },
+    [state.selected],
+  )
+
+  const options: Options = {
+    ...defaultOptions,
+    locale: props.locale,
+    weekStartsOn: props.weekStartsOn,
+  }
+
+  const setFocusToCalendar = () => {
     window.requestAnimationFrame(() => {
-      if (this.dateRefs[this.state.date.toISOString()]) {
-        this.dateRefs[this.state.date.toISOString()].focus()
+      if (dateRefs[state.date.toISOString()]) {
+        dateRefs[state.date.toISOString()].focus()
       }
     })
   }
 
-  private handleKeyDown = (e: React.KeyboardEvent) => {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
     const action = keyToAction(e.key)
 
     if (action) {
-      const d = this.dispatch(action).date
-      this.setFocusToDate(d)
+      dispatch(action)
     }
   }
 
-  private getCalendarProps = (): CalendarProps => {
+  const getCalendarProps = (): CalendarProps => {
     return {
-      onKeyDown: this.handleKeyDown,
+      onKeyDown: handleKeyDown,
       tabIndex: 0,
     }
   }
 
-  private getPrevMonthProps = (): MonthProps => {
+  const getPrevMonthProps = (): MonthProps => {
     return {
-      onClick: () => this.dispatch({ type: 'PrevMonth' }),
+      onClick: () => dispatch({ type: 'PrevMonth' }),
       'aria-label': `Go back 1 month`,
       role: 'button',
     }
   }
 
-  private getNextMonthProps = (): MonthProps => {
+  const getNextMonthProps = (): MonthProps => {
     return {
-      onClick: () => this.dispatch({ type: 'NextMonth' }),
+      onClick: () => dispatch({ type: 'NextMonth' }),
       'aria-label': `Go ahead 1 month`,
       role: 'button',
     }
   }
 
-  private getDateProps = (calendarDay: CalendarDay): DateProps => {
+  const getDateProps = (calendarDay: CalendarDay): DateProps => {
     return {
-      onClick: () =>
-        this.dispatch({ type: 'SelectDate', date: calendarDay.date }),
+      onClick: e => {
+        e.preventDefault()
+        dispatch({ type: 'SelectDate', date: calendarDay.date })
+      },
       'aria-label': calendarDay.date.toDateString(),
       'aria-pressed': calendarDay.selected,
       role: 'button',
       tabIndex: calendarDay.current ? 0 : -1,
       ref: (el: HTMLElement | null) => {
         if (el) {
-          this.dateRefs[calendarDay.date.toISOString()] = el
+          dateRefs[calendarDay.date.toISOString()] = el
         }
       },
     }
   }
 
-  private buildCalendar = (state = this.state) =>
-    buildCalendar(state, this.options)
-
-  private dispatch(action: Action) {
-    const newState = reducer(this.state, action, this.options)
-
-    if (
-      newState.selected &&
-      this.state.selected !== newState.selected &&
-      this.props.onChange
-    ) {
-      this.props.onChange(newState.selected)
-    }
-
-    this.setState(newState)
-
-    return newState
-  }
-
-  private get options() {
-    return {
-      ...defaultOptions,
-      weekStartsOn: this.props.weekStartsOn,
-    }
+  return {
+    ...buildCalendar(state, options),
+    selectDate: (date: string | number | Date) =>
+      dispatch({ type: 'SelectDate', date }),
+    prevDay: () => dispatch({ type: 'PrevDay' }),
+    nextDay: () => dispatch({ type: 'NextDay' }),
+    prevWeek: () => dispatch({ type: 'PrevWeek' }),
+    nextWeek: () => dispatch({ type: 'NextWeek' }),
+    prevMonth: () => dispatch({ type: 'PrevMonth' }),
+    nextMonth: () => dispatch({ type: 'NextMonth' }),
+    getCalendarProps,
+    getDateProps,
+    getNextMonthProps,
+    getPrevMonthProps,
+    handleKeyDown,
+    setFocusToCalendar,
+    setFocusToDate,
   }
 }
+
+export const Repick: React.FunctionComponent<RepickProps & Children> = ({
+  children,
+  ...props
+}) => {
+  const childProps = useRepick(props)
+
+  return children(childProps)
+}
+
+export default Repick
