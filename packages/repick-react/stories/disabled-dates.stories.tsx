f91cc2fba5c66b47446040abb22380bf735f2c65
Aki Seppänen <aki.seppanen@newthings.co>
2020-03-13 10:18:54 +0200

    Enable storybook for examples

---
+++ packages/repick-react/stories/disabled-dates.stories.tsx
@@ -0,0 +1,75 @@
+import classnames from 'classnames'
+import format from 'date-fns/format'
+import * as React from 'react'
+import { useRepick } from '../src'
+
+import { ArrowLeft, ArrowRight } from './arrows'
+
+export default {
+  title: 'Repick React',
+}
+
+export const DisabledDates = () => {
+  const disabledDates = Array.apply(null, Array(10)).map(
+    (_, i) => new Date(new Date().setDate(10 + i)),
+  )
+
+  const {
+    selected,
+    days,
+    monthLong,
+    year,
+    weekdays,
+    getDateProps,
+    getPrevMonthProps,
+    getNextMonthProps,
+    getCalendarProps,
+  } = useRepick({ weekStartsOn: 1, disabledDates })
+
+  return (
+    <>
+      <input
+        type="text"
+        value={selected ? format(selected, 'MM/dd/yyyy') : ''}
+        readOnly
+      />
+      <div {...getCalendarProps()} className="calendar">
+        <div className="calendarMonths">
+          <div {...getPrevMonthProps()} className="calendarMonthPrev">
+            <ArrowLeft />
+          </div>
+          <div className="calendarCurrentMonth">
+            {monthLong} {year}
+          </div>
+          <div {...getNextMonthProps()} className="calendarMonthNext">
+            <ArrowRight />
+          </div>
+        </div>
+        <div className="calendarWeekdays">
+          {weekdays.map(weekday => (
+            <div key={`weekday-${weekday.short}`} className="calendarWeekday">
+              {weekday.short}
+            </div>
+          ))}
+        </div>
+        <div className="calendarDayContainer">
+          {days.map(calendarDay => (
+            <button
+              {...getDateProps(calendarDay)}
+              key={calendarDay.date.toISOString()}
+              className={classnames('calendarDay', {
+                nextMonth: calendarDay.nextMonth,
+                prevMonth: calendarDay.prevMonth,
+                selected: calendarDay.selected,
+                today: calendarDay.today,
+                disabled: calendarDay.disabled,
+              })}
+            >
+              {calendarDay.day}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
